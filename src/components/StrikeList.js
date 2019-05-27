d931926b079118a98e314315a860aaf970c5be56
Will Gutierrez <will.gutierrez@gmail.com>
2019-05-26 21:24:54 -0700

    maps data to a React Bootstrap Table component

--- src/components/StrikeList.js
+++ src/components/StrikeList.js
@@ -1,12 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Table } from 'react-bootstrap'
 
 class StrikeList extends React.Component {
 	render() {
+    let strikes = []
+
+    strikes = this.props.meteoriteData.map((strike, index) => {
+      return (
+        <tr key={strike.id}>
+          <td>{strike.name}</td>
+          <td>{strike.id}</td>
+          <td>{strike.nametype}</td>
+          <td>{strike.recclass}</td>
+          <td>{strike.mass}</td>
+          <td>{strike.fall}</td>
+          <td>{strike.year}</td>
+          <td>{strike.reclat}</td>
+          <td>{strike.reclong}</td>
+        </tr>
+      );
+    })
+
 		return (
-			<div>
-        Here is the list of strikes!
-      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Id</th>
+            <th>Name Type</th>
+            <th>Rec Class</th>
+            <th>Mass (g)</th>
+            <th>Fall</th>
+            <th>Year</th>
+            <th>Latitude</th>
+            <th>Longitude</th>
+          </tr>
+        </thead>
+        <tbody>
+          {strikes}
+        </tbody>
+      </Table>
 		);
 	}
 }
