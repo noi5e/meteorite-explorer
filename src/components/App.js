ce81a1e760f2498439d0cc9c4d64c5035875e13f
Will Gutierrez <will.gutierrez@gmail.com>
2019-05-26 19:52:03 -0700

    adds API call to NASA in componentDidMount

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      meteoriteData: []
+    }
+  }
+
+  componentDidMount() {
+    axios({
+      method: 'get',
+      url: 'https://data.nasa.gov/resource/gh4g-9sfh.json',
+      // responseType: 'stream',
+      data: {
+        "$limit": 5000,
+        "$$app_token": "hwz12X1ARsE8InvJVrwRpOAY0"
+      }
+    })
+      .then((response) => {
+        this.setState({
+          meteoriteData: response.data
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello World!
+      </div>
+    );
+  }
 }
 
 export default App;
